f7238041d2204d550507710a4557502eb51943c7
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-29 11:45:39 -0700

    Java projects

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var A = "This is a String!";
+document.write(A);
+var B = "quote";
+document.write("Lisa told Bart, \"Knock it off, Bart! or i'll tell dad!\"<br>\"Eat me shorts!\"Bart responded.");
+var C = "quoteTwo";
+document.write("\"Be who you are and say what you feel," + "because those mind don\'t matter and those matter don\'t mind.\""+"-Dr. Seuss");
+
+var D = "B" + "C";
+document.write(D);
+
+var Family = "The Arezzinins", Dad = "Jeremiah", Mom = "Jan", Daughter = "Penny", Son = "Peter";
+document.write(Mom);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue")
+document.write(blues);
+
+document.write(3 + 3);
+
+function My_First_Fucntion() { //difining a function and naming it
+    var str = "This text is green!"; //defining a variable and giving it a string value
+    var result = str.fontcolor("green"); //using the fontcolor method on str variables
+    document.getElementById("Green_Text") .innerHtml = result; //putting the value // of result into HTML element with "Green_text" id
+}
