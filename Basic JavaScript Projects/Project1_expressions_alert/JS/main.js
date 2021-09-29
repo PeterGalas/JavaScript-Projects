14d26f88f628d1ae5d12c8c8f1d46cdaf0246be3
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-29 19:59:12 -0400

    update

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,21 @@
-window.alert("Dont Forget to click OK!");
-document.write("YOU HAVE TO CLICK THE BUTTON!!!!");
+window.alert("Hello welcome")
+var A = "This is a String!";
+document.write(A);
 
-function My_First_Function () { // Defining a function and naming it
-    var str = "This text is green!"; //Defining a variable and giving it a sring value
-    var result = str.fontcolor("green"); //using the fontcolor method on str variable
-    document.getElementById("Green_Text").innerHTML = result; // putting the value of the result into html element with Green_Text id
+document.write("Lisa told Bart, \"Knock it off, Bart! or i'll tell dad!\"<br>\"Eat me shorts!\"Bart responded.");
+
+document.write("\"Be who you are and say what you feel," + "because those mind don\'t matter and those matter don\'t mind.\""+"-Dr. Seuss");
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue")
+document.write(blues);
+
+document.write("My name is Peter");
+
+function My_First_Function () {
+    var str = "this text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text") .innerHTML = result;
 }
+
+
