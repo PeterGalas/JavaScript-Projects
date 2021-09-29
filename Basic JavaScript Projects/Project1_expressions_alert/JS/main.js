a4c3109bf5a5d72efaeb3f39283d95caf7a7f987
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-29 17:17:51 -0400

    k

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,28 +1,8 @@
-window.alert("Hello welcome")
-var A = "This is a String!";
-document.write(A);
+window.alert("Dont Forget to click OK!");
+document.write("YOU HAVE TO CLICK THE BUTTON!!!!");
 
-document.write("Lisa told Bart, \"Knock it off, Bart! or i'll tell dad!\"<br>\"Eat me shorts!\"Bart responded.");
-
-document.write("\"Be who you are and say what you feel," + "because those mind don\'t matter and those matter don\'t mind.\""+"-Dr. Seuss");
-
-var blues = "I have the blues.";
-var blues = blues.fontcolor("blue")
-document.write(blues);
-
-document.write("My name is Peter");
-
-function My_First_Function() { //difining a function and naming it
-    var str = "This text is green!"; //defining a variable and giving it a string value
-    var result = str.fontcolor("green"); //using the fontcolor method on str variables
-    document.getElementById("Green_Text") .innerHtml = result; //putting the value // of result into HTML element with "Green_text" id
+function My_First_Function () { // Defining a function and naming it
+    var str = "This text is green!"; //Defining a variable and giving it a sring value
+    var result = str.fontcolor("green"); //using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; // putting the value of the result into html element with Green_Text id
 }
-
-
-
-function myFunction() {
-    var sentence = "I am Learning";
-    sentence += " a lot from this JavaScript course!";
-    document.getElementById("Concatencate").innerHTML = sentence;
-}
-
