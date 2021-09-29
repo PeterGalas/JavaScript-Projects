cbbac45fc7712f6faf3f818f6e4bb8357d520d91
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-29 16:22:40 -0400

    draft

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -15,7 +15,7 @@ document.write("My name is Peter");
 function My_First_Function() { //difining a function and naming it
     var str = "This text is green!"; //defining a variable and giving it a string value
     var result = str.fontcolor("green"); //using the fontcolor method on str variables
-    document.getElementById("Green_Text").innerHtml = result; //putting the value // of result into HTML element with "Green_text" id
+    document.getElementById("Green_Text") .innerHtml = result; //putting the value // of result into HTML element with "Green_text" id
 }
 
 
