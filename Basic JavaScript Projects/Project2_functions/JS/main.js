098d85fccef6bd03107cb2b3e151ca8149d5bcb6
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-30 14:40:22 -0400

    math problems

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,12 @@
-function myFunction(p1, p2) {
-    return p1 * p2;
-    }
-    
-document.getElementById("demo").innerHTML = myFunction(4, 3);
+function myFunction() { //function allows you to connect sentence strings
+    var sentence = "I im learning"; 
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+}
 
+// var foo = document.getElementByTagName("p"); finding all paragraph elements in html 
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,47 @@
+function addition_Function() {
+    var addition = 5 + 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + addition;
+}
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math() {
+    var simple_Math = (1+2) * 10 / 2-5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math; 
+}
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math").innerHTML = -x;
+}
+//adds 1 digit
+var X = 5;
+X++;
+document.write(X);
+//takes 1 digit away
+var X = 5.25;
+X--;
+document.write(X);
+
+//number 0-1
+window.alert(Math,random());
+//number between 0-100
+window.alert(Math,random() * 100);
