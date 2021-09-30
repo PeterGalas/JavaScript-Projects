3f92bd1d4bf7af64e338b0d26d2e7025ea57a4e2
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-09-30 10:48:12 -0400

    error

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,6 @@
-function myFunction() { //calling function
-    var sentence = "I am learning"; //concatentating the two sentences by adding +=
-    sentence += " a lot, from this book!";
-    document.getElementById("Concatencate") .innerHTML = sentence; //getting element by id
-}
-
 function myFunction(p1, p2) {
     return p1 * p2;
     }
-document.getElementbyId("demo").innerHTML = myFunction(4, 3);
+    
+document.getElementById("demo").innerHTML = myFunction(4, 3);
 
