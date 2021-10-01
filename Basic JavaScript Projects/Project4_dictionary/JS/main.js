e0a3bc8f77f333da82036ba05c9e050026a241c7
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-10-01 13:55:32 -0400

    push

--- Basic JavaScript Projects/Project4_dictionary/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionary/JS/main.js
@@ -1,12 +1,13 @@
 function my_Dictionary() {
     var Animal = {
         Species:"Dog",
-        Color:"Black",
-        Breed: "French Bulldog",
+        Color:"Black with White",
+        Breed:"French Bulldog",
         Age:"2",
         Sound:"Bark!"
     };
-    delete Animal.Sound; //takes away with a delete variable(Sound=Bark) gives a undifined!
-    document.getElementById("Dictionary").innerHTML = Animal.Breed + Animal.Sound; //key value pairs after your variable.any variable you choose for an output.
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Breed + Animal.Sound;
 }
+
 
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,7 @@
+document.write(typeof "Name");//gives data type of a variable (output string)
+
+document.write(typeof 35);//gives output number type
+
+document.write("10" + 5);// "Type coerion" string plus number convertion without this type it would return an error.
+
+
