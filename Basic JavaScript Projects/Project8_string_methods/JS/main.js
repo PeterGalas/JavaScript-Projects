baac19052e504d5f285de5649a732071f507db89
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-10-04 11:49:29 -0400

    project 8

---
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,24 @@
+function full_Sentence() {//concatentating strings
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+function slice_Method() {//gives a pice of data
+    var Sentence = "All work and no play makes Jonny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function string_Method() {//returns your variable
+    var X =182;
+    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+function percision_Method() {//formats a number to a specified legth
+    var X = 12938.3012987376112;
+    document.getElementById("Percision").innerHTML = X.toPrecision(10);
+}
