52bd4ffac329b3b3d542c9bc95475a43427ea610
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-10-05 15:42:02 -0400

    fix

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-// counting 1 to 10
+// count to 10 var 
 function count_To_Ten() {
     var Digit = "";
     var X = 1;
@@ -9,16 +9,66 @@ function count_To_Ten() {
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
 
-let str ="Hello World";
-document.getElementById("demo").innerHTML = str.length;;
-str.length // retuns 12
-
-var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-var Content = ""
+// Instrument printing each item
+var Instruments = ["Guitar","Drums","Piano","Bass","Violin","Trumpet","Flute"];
+var Content = "";
 var Y;
 function for_Loop() {
     for (Y = 0; Y < Instruments.length; Y++) {
-    Content += Instruments[Y] + "<br>";
+        Content += Instruments[Y] + "<br>";
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+
+function cat_pics(){
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "in this picture, the cat is " +
+    Cat_Picture[0] + "."; //giving you any choice of cat picture []
+}
+
+//block scope
+//function myFunction() { 
+    //var carName = "E30";
+    //document.getElementById("Example").innerHTML = carName;
+//}
+
+function constant_function(){
+    const Musical_Instrument = {type:"guitar",brand:"Fender",color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The Cost of the " +
+    Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+//outputs 82, 33, 82
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+
+//var X = 82;
+//document.write(X);
+{
+    var X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
