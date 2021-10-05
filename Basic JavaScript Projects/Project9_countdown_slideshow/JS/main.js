bab7f3997d90347d19b8bc0b398fb3727b4dfe00
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-10-04 22:08:41 -0400

    project 9

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -3,13 +3,41 @@ function countdown() {
 
     function tick() {
         seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
+        TimeRanges.innerHTML = seconds;
+        var Time = setTimeout(tick, 1000);
         if (seconds == -1) {
-            alert("Time's up!");
-            clearTimeout(time);
-            timer.innerHTML = "";
+            alert("Time's UP!");
+            clearTimeout(Time);
+            TimeRanges.innerHTML = "";
         }
     }
     tick();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
